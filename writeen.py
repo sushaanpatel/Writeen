43614266532f538a005480e9a2201a58364d7be7
sushaanpatel <sushaanpatel@gmail.com>
2021-05-05 01:28:27 +0530

    adding log-in and sign-up page, completing create page

--- writeen.py
+++ writeen.py
@@ -11,7 +11,7 @@ con = mysql.connector.connect(
 
 @app.route('/', methods = ["POST", "GET"])
 def index():
-    return render_template('index.html')
+    return render_template('create.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
